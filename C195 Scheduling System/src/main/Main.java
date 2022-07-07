1f478710129cf8008c459d80e7a2e92d4993bca6
KeithHudson1 <hudson.keith@live.com>
2022-07-07 06:16:46 -0700

    Test save from VM

--- C195 Scheduling System/src/main/Main.java
+++ C195 Scheduling System/src/main/Main.java
@@ -14,8 +14,9 @@ import java.util.ResourceBundle;
 public class Main extends Application {
 
     public static void main(String[] args) {
+        System.out.println("Chirp");
 //        Locale.setDefault(new Locale("en"));
-//        Locale.setDefault(new Locale("fr")); // This sets your local system language to French.
+        Locale.setDefault(new Locale("fr")); // This sets your local system language to French.
 //        Locale.setDefault(new Locale("es"));
 //        Locale.setDefault(new Locale("de"));
         DBConnection.openConnection();
